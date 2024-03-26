5e4d75d89f47adc47deb6d313bf99e6f53a943f2
Sekvoya <82507998+Tbma1@users.noreply.github.com>
2024-03-26 16:14:16 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,38 +6,24 @@ const ctx = canvas.getContext('2d');
 
 let isDrawing = false;
 
-function handleImage(e){
+function handleImage(e) {
     const reader = new FileReader();
-    reader.onload = function(event){
+    reader.onload = function(event) {
         const img = new Image();
-        img.onload = function(){
+        img.onload = function() {
             canvas.width = img.width;
             canvas.height = img.height;
             ctx.drawImage(img, 0, 0);
-            canvas.classList.remove('hidden'); // Показываем canvas
-            scrollToTopButton.classList.remove('hidden'); // Показываем кнопку "Наверх"
         }
         img.src = event.target.result;
     }
-    reader.readAsDataURL(e.target.files[0]);     
+    reader.readAsDataURL(e.target.files[0]);
 }
 
-canvas.addEventListener('mousedown', (e) => {
+canvas.addEventListener('mousedown', function(e) {
     isDrawing = true;
     ctx.beginPath();
     ctx.moveTo(e.offsetX, e.offsetY);
 });
 
-canvas.addEventListener('mousemove', (e) => {
-    if (isDrawing === true) {
-        ctx.lineTo(e.offsetX, e.offsetY);
-        ctx.stroke();
-    }
-});
-
-canvas.addEventListener('mouseup', (e) => {
-    if (isDrawing === true) {
-        ctx.lineTo(e.offsetX, e.offsetY);
-        ctx.stroke();
-        ctx.closePath();
-        isDrawing = false
+canvas.addEventListener
