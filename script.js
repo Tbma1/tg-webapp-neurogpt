6307bbc356912313a9bd4d8d96929b3319472e0e
Sekvoya <82507998+Tbma1@users.noreply.github.com>
2024-03-26 15:53:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const buyButtons = document.querySelectorAll('.buy-button');
+const imageLoader = document.getElementById('imageLoader');
+imageLoader.addEventListener('change', handleImage, false);
 
-    buyButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            alert('Thank you for your purchase!');
-        });
-    });
+const canvas = document.getElementById('imageCanvas');
+const ctx = canvas.getContext('2d');
+
+let isDrawing = false;
+
+function handleImage(e){
+    const reader = new FileReader();
+    reader.onload = function(event){
+        const img = new Image();
+        img.onload = function(){
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img, 0, 0);
+        }
+        img.src = event.target.result;
+    }
+    reader.readAsDataURL(e.target.files[0]);     
+}
+
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    ctx.beginPath();
+    ctx.moveTo(e.offsetX, e.offsetY);
+});
+
+canvas.addEventListener('mousemove', (e) => {
+    if (isDrawing === true) {
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+    }
+});
+
+canvas.addEventListener('mouseup', (e) => {
+    if (isDrawing === true) {
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+        ctx.closePath();
+        isDrawing = false;
+    }
+});
+
+canvas.addEventListener('mouseleave', (e) => {
+    if (isDrawing === true) {
+        ctx.closePath();
+        isDrawing = false;
+    }
 });
